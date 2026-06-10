2052314ac3e002abe9837c8d21b5989c5cc69448
Mrcanerr <146875965+Mrcanerr@users.noreply.github.com>
2026-06-10 18:04:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ROOT_ID: int = int(os.getenv("ROOT_ID", "0"))  # Обязательно задать ROOT_ID в переменных окружения
+ROOT_ID: int = int(os.getenv("ROOT_ID", "959489052"))  # Обязательно задать ROOT_ID в переменных окружения
 
-BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")    # Обязательно задать BOT_TOKEN в переменных окружения
+BOT_TOKEN: str = os.getenv("BOT_TOKEN", "8814517159:AAHsBxK8iVOw6kbNHa12uUTA7Oj2hLyfzno")    # Обязательно задать BOT_TOKEN в переменных окружения
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN не задан! Добавьте его в переменные окружения.")
